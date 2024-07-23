4d48a9be54c0503ee1ecd81dbe23255072c32a85
patrikpimentel <138342191+patrikoliver@users.noreply.github.com>
2024-07-23 12:20:48 -0300

    Atualizado desconto da classe DescontoAnoNovo

--- src/desconto/DescontoAnoNovo.java
+++ src/desconto/DescontoAnoNovo.java
@@ -3,7 +3,7 @@ package desconto;
 public class DescontoAnoNovo implements DescontoStrategy {
     @Override
     public double aplicarDesconto(double valor) {
-        //15% de desconto
-	return valor * 0.85; 
+        //35% de desconto
+	return valor * 0.65; 
     }
 }
